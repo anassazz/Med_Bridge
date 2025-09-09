354c9df02033beab339adb6607e071a2c5bcaae5
anassazz <anass.azz2030@gmail.com>
2025-09-09 15:56:09 +0100

    Add server setup with essential middleware and routing

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import userRoutes from "./routes/userRoutes.js";
+import { errorMiddleware } from "./middleware/errorMiddleware.js";
+import cloudinary from "cloudinary";
+import messageRoutes from "./routes/messageRoutes.js";
+import appointmentRoutes from "./routes/appointmentRoutes.js";
+const app = express();
